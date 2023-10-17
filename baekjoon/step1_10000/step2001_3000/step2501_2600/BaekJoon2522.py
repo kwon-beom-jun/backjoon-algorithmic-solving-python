0a0c5853381c859e51fb86badc0b921090d85da5
kwon-beom-jun <qjawns0618@naver.com>
2023-10-17 23:27:57 +0900

    23.10.17 BaekJoon Upload

---
+++ baekjoon/step1_10000/step2001_3000/step2501_2600/BaekJoon2522.py
@@ -0,0 +1,19 @@
+num = int(input())
+str_list = []
+
+for i in range(num) :
+    for _ in range(num-i-1) :
+        str_list.append(" ")
+    for _ in range(i+1) :
+        str_list.append("*")
+    str_list.append("\n")
+    
+for i in range(num) :
+    for _ in range(i+1) :
+        str_list.append(" ")
+    for _ in range(num-i-1) :
+        str_list.append("*")
+    str_list.append("\n")
+
+
+print(''.join(str_list))
---
+++ baekjoon/step1_10000/step2001_3000/step2901_3000/BaekJoon2935.py
@@ -0,0 +1,5 @@
+A = int(input())
+operator = str(input())
+B = int(input())
+
+print(A * B if operator == "*" else A + B)
